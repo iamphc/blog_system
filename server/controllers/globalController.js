64072bc016ce2dce95b33ec0e733206613bea6bf
panghaichuan <panghaichuan@edusoho.com>
2021-04-01 22:31:29 +0800

    feat: #20210330 客户端添加axios请求、响应拦截器，服务端添加bcryptjs用来加密注册用户的密码

--- server/controllers/globalController.js
+++ server/controllers/globalController.js
@@ -1,19 +1,25 @@
+const User = require('../database/models/User');
+
+// TODO:无
 exports.global_homepage = (req, res, next) => {
   res.send('global homepage');
 }
 
-exports.global_register = (req, res, next) => {
-  req.body = {
-    a:1,
-    b:2
-  }
-  res.send('global register');
+// TODO:创建用户
+exports.global_register = async(req, res, next) => {
+  const user = await User.create({
+    userName: req.body,
+    userPwd: req.body.userPwd
+  })
+  res.send(user);
 }
 
+// TODO:用户登录验证
 exports.global_login = (req, res, next) => {
   res.send('global user login');
 }
 
+// TODO:获取菜单（顶部+侧边）
 exports.global_get_menu_items = (req, res, next) => {
   res.send('global top-menu/side-menu');
 }
--- server/database/models/User.js
+++ server/database/models/User.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');   // 用户注册时对密码加密
+
 const User = new mongoose.Schema({
     userName: {
         type: String,
         required: true,
         minLength: 5,
-        maxLength: 15
+        maxLength: 15,
+        unique: true    // 用户名字段唯一
     },
     userPwd: {
         type: String,
         required: true,
         minLength: 5,
-        maxLength: 15
+        maxLength: 15,
+        set(pwd) {
+            return bcryptjs.hashSync(pwd, 10);
+        }
     },
     userType: {
         type: Number,
