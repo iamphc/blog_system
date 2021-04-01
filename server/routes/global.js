2648c5a33303aaafff9de110a5d210f7c162d85b
panghaichuan <panghaichuan@edusoho.com>
2021-04-01 21:15:57 +0800

    feat: #20210330 服务端调整目录

--- server/routes/global.js
+++ server/routes/global.js
@@ -1,23 +1,17 @@
 const express = require('express');
+const global_controller = require('../controllers/globalController');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.send('global homepage');
-});
+// TODO:无
+router.get('/', global_controller.global_homepage);
 
 // TODO:创建用户
-router.post('/user/register', (req, res, next) => {
-    res.send(req);
-});
+router.post('/user/register', global_controller.global_register);
 
 // TODO:用户登录验证
-router.post('/user/login', (req, res, next) => {
-    res.send('global user login');
-});
+router.post('/user/login', global_controller.global_login);
 
 // TODO:获取菜单（顶部+侧边）
-router.get('/menusItems', (req, res, next) => {
-    res.send('global top-menu/side-menu');
-});
+router.get('/menusItems', global_controller.global_get_menu_items);
 
 module.exports = router;
