78ad986fd47903a897bc61883691f78368c5c28c
panghaichuan <panghaichuan@edusoho.com>
2021-04-17 10:52:06 +0800

    fix: #20210417 客户端，文章上传组件样式修改

--- client/src/blog/components/EditArticle.vue
+++ client/src/blog/components/EditArticle.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="edit-article_wrapper"> 
-    <span class="article-title" contenteditable="true" data-placeholder="标题"></span>
+    <div class="article-title" contenteditable="true" data-placeholder="标题"></div>
     <el-divider></el-divider>
-    <textarea class="article-maintain" contenteditable="true" data-placeholder="请输入正文"></textarea>
+    <div class="article-maintain" contenteditable="true" data-placeholder="请输入正文"></div>
     <el-divider></el-divider>
     <div class="operation-zone">
       <el-button
