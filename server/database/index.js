a3ccf99b6d4b776e20896cecee120adee84b596a
panghaichuan <panghaichuan@edusoho.com>
2021-05-05 09:22:04 +0800

    feat: #20210504 服务端，删除userSetting表，合并到user表。user表删除operationToken字段。

--- server/database/index.js
+++ server/database/index.js
@@ -1,7 +1,6 @@
 require('./createCollection') // 连接数据库
 require('./bin')  // 执行初始化数据表脚本
- 
-exports.UserSetting = require('./models/UserSetting')
+  
 exports.BlogArticle = require('./models/BlogArticle')
 exports.BlogTheme = require('./models/BlogTheme')
 exports.BlogNote = require('./models/BlogNote')
--- server/database/models/User.js
+++ server/database/models/User.js
@@ -2,33 +2,35 @@ const mongoose = require('mongoose');
 const bcryptjs = require('bcryptjs'); // 用户注册时对密码加密 
 
 const User = new mongoose.Schema({
-    userName: {
-        type: String,
-        required: true,
-        minLength: 5,
-        maxLength: 15,
-        trim: true,
-        unique: true, // 用户名字段唯一  
-    },
-    userPwd: {
-        type: String,
-        required: true,
-        set(pwd) {
-            const salt = bcryptjs.genSaltSync(10) // 生成加密盐
-            return bcryptjs.hashSync(pwd, salt)
-        },
-        select: false // 不查询该字段
-    },
-    userType: {
-        type: Number,
-        default: 1, // 用户类型：0（普通用户）、1（管理员）
-    },
-    userLoginTime: {
-        type: Date
-    },
-    operationToken: {
-        type: String
-    }
+	userName: {
+		type: String,
+		required: true,
+		minLength: 5,
+		maxLength: 15,
+		trim: true,
+		unique: true, // 用户名字段唯一  
+	},
+	userPwd: {
+		type: String,
+		required: true,
+		set(pwd) {
+			const salt = bcryptjs.genSaltSync(10) // 生成加密盐
+			return bcryptjs.hashSync(pwd, salt)
+		},
+		select: false // 不查询该字段
+	},
+	userType: {
+		type: Number,
+		default: 1, // 用户类型：0（普通用户）、1（管理员）
+	},
+	userLoginTime: {
+		type: Date
+	}, 
+	// 博客主题颜色(name)，默认为no-setting
+	blogTheme: {
+		type: String,
+		default: 'no-setting'
+	}
 })
 
 module.exports = mongoose.model('User', User);
