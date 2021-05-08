9cc185b303e2d44d742dd336acd17391fa3e6ad2
panghaichuan <panghaichuan@edusoho.com>
2021-05-08 16:31:55 +0800

    feat: #20210508 删除多余注释

--- client/src/blog/homepagePart/TagCloud.vue
+++ client/src/blog/homepagePart/TagCloud.vue
@@ -1,26 +1,9 @@
 <template>
   <section class="tag-clouds">
     <div class="tag-clouds__header">
-      <span class="title">随机标签云</span>
+      <span class="title">标签云</span>
     </div> 
     <div class="tag-cloud-list" ref="tagCloud"></div>
-    <!-- <ul class="tag-cloud-list">
-      <li class="tag-list-item"><a class="tag-link" href="#">lorem</a></li>
-      <li class="tag-list-item"><a class="tag-link" href="#">lorem</a></li>
-      <li class="tag-list-item"><a class="tag-link" href="#">lorem</a></li>
-      <li class="tag-list-item"><a class="tag-link" href="#">lorem</a></li>
-      <li class="tag-list-item"><a class="tag-link" href="#">lorem</a></li>
-      <li class="tag-list-item"><a class="tag-link" href="#">lorem</a></li>
-      <li class="tag-list-item"><a class="tag-link" href="#">lorem</a></li>
-      <li class="tag-list-item"><a class="tag-link" href="#">lorem</a></li>
-      <li class="tag-list-item"><a class="tag-link" href="#">lorem</a></li>
-      <li class="tag-list-item"><a class="tag-link" href="#">lorem</a></li>
-      <li class="tag-list-item"><a class="tag-link" href="#">lorem</a></li>
-      <li class="tag-list-item"><a class="tag-link" href="#">lorem</a></li>
-      <li class="tag-list-item"><a class="tag-link" href="#">lorem</a></li>
-      <li class="tag-list-item"><a class="tag-link" href="#">lorem</a></li>
-      <li class="tag-list-item"><a class="tag-link" href="#">lorem</a></li>
-    </ul>  -->
   </section>
 </template>
 
